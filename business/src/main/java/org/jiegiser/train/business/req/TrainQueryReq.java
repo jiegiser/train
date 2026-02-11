3c5b5a9fa61572e3d8583c6b0411fe23f61acf2e
jiegiser <changjie@jouav.com>
2026-02-11 08:55:28 +0800

    feat: 新增车次接口

---
+++ business/src/main/java/org/jiegiser/train/business/req/TrainQueryReq.java
@@ -0,0 +1,12 @@
+package org.jiegiser.train.business.req;
+
+import org.jiegiser.train.common.req.PageReq;
+
+public class TrainQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "TrainQueryReq{" +
+                "} " + super.toString();
+    }
+}
