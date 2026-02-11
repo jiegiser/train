5062526a392d9ba3485124f5276b95d5247588f4
jiegiser <changjie@jouav.com>
2026-02-11 08:26:10 +0800

    feat: 生成 admin 相关代码

---
+++ business/src/main/java/org/jiegiser/train/business/req/StationQueryReq.java
@@ -0,0 +1,12 @@
+package org.jiegiser.train.business.req;
+
+import org.jiegiser.train.common.req.PageReq;
+
+public class StationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "StationQueryReq{" +
+                "} " + super.toString();
+    }
+}
