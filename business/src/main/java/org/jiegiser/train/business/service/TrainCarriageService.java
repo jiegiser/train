6c4fce6cb0f90167da34f2e9d43a04b80423b55c
jiegiser <changjie@jouav.com>
2026-02-12 10:35:35 +0800

    feat: 车次、车厢、车站增加存在性校验

--- business/src/main/java/org/jiegiser/train/business/service/TrainCarriageService.java
+++ business/src/main/java/org/jiegiser/train/business/service/TrainCarriageService.java
@@ -1,19 +1,23 @@
 package org.jiegiser.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
-import org.jiegiser.train.common.resp.PageResp;
-import org.jiegiser.train.common.util.SnowUtil;
+import jakarta.annotation.Resource;
 import org.jiegiser.train.business.domain.TrainCarriage;
 import org.jiegiser.train.business.domain.TrainCarriageExample;
+import org.jiegiser.train.business.enums.SeatColEnum;
 import org.jiegiser.train.business.mapper.TrainCarriageMapper;
 import org.jiegiser.train.business.req.TrainCarriageQueryReq;
 import org.jiegiser.train.business.req.TrainCarriageSaveReq;
 import org.jiegiser.train.business.resp.TrainCarriageQueryResp;
-import jakarta.annotation.Resource;
+import org.jiegiser.train.common.exception.BusinessException;
+import org.jiegiser.train.common.exception.BusinessExceptionEnum;
+import org.jiegiser.train.common.resp.PageResp;
+import org.jiegiser.train.common.util.SnowUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -30,8 +34,21 @@ public class TrainCarriageService {
 
     public void save(TrainCarriageSaveReq req) {
         DateTime now = DateTime.now();
+
+        // 自动计算出列数和总座位数
+        List<SeatColEnum> seatColEnums = SeatColEnum.getColsByType(req.getSeatType());
+        req.setColCount(seatColEnums.size());
+        req.setSeatCount(req.getColCount() * req.getRowCount());
+
         TrainCarriage trainCarriage = BeanUtil.copyProperties(req, TrainCarriage.class);
         if (ObjectUtil.isNull(trainCarriage.getId())) {
+
+            // 保存之前，先校验唯一键是否存在
+            TrainCarriage trainCarriageDB = selectByUnique(req.getTrainCode(), req.getIndex());
+            if (ObjectUtil.isNotEmpty(trainCarriageDB)) {
+                throw new BusinessException(BusinessExceptionEnum.BUSINESS_TRAIN_CARRIAGE_INDEX_UNIQUE_ERROR);
+            }
+
             trainCarriage.setId(SnowUtil.getSnowflakeNextId());
             trainCarriage.setCreateTime(now);
             trainCarriage.setUpdateTime(now);
@@ -39,6 +56,19 @@ public class TrainCarriageService {
         } else {
             trainCarriage.setUpdateTime(now);
             trainCarriageMapper.updateByPrimaryKey(trainCarriage);
+        }
+    }
+
+    private TrainCarriage selectByUnique(String trainCode, Integer index) {
+        TrainCarriageExample trainCarriageExample = new TrainCarriageExample();
+        trainCarriageExample.createCriteria()
+                .andTrainCodeEqualTo(trainCode)
+                .andIndexEqualTo(index);
+        List<TrainCarriage> list = trainCarriageMapper.selectByExample(trainCarriageExample);
+        if (CollUtil.isNotEmpty(list)) {
+            return list.get(0);
+        } else {
+            return null;
         }
     }
 
@@ -71,6 +101,11 @@ public class TrainCarriageService {
         trainCarriageMapper.deleteByPrimaryKey(id);
     }
 
+    /**
+     * 查找当前车次下的所有的车厢
+     * @param trainCode
+     * @return
+     */
     public List<TrainCarriage> selectByTrainCode(String trainCode) {
         TrainCarriageExample trainCarriageExample = new TrainCarriageExample();
         trainCarriageExample.setOrderByClause("`index` asc");
