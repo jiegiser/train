d9caa4503b714da6e49e2da55fd12cf29ed61d4a
jiegiser <changjie@jouav.com>
2026-02-15 08:51:04 +0800

    feat: 新增每日车厢数据接口

---
+++ business/src/main/java/org/jiegiser/train/business/req/DailyTrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package org.jiegiser.train.business.req;
+
+import org.jiegiser.train.common.req.PageReq;
+
+public class DailyTrainCarriageQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainCarriageQueryReq{" +
+                "} " + super.toString();
+    }
+}
