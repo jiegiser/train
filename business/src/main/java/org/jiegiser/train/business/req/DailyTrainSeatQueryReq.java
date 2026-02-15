04146a586376542f7daf7aa075d85f30afa578b8
jiegiser <changjie@jouav.com>
2026-02-15 09:03:44 +0800

    feat: 新增每日座位相关接口

---
+++ business/src/main/java/org/jiegiser/train/business/req/DailyTrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package org.jiegiser.train.business.req;
+
+import org.jiegiser.train.common.req.PageReq;
+
+public class DailyTrainSeatQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainSeatQueryReq{" +
+                "} " + super.toString();
+    }
+}
