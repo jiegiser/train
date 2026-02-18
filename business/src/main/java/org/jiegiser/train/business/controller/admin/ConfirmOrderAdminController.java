95e5e47457f73ee0ac8324ae0a5c507498012817
jiegiser <changjie@jouav.com>
2026-02-18 08:38:48 +0800

    feat: 新增订单信息接口

---
+++ business/src/main/java/org/jiegiser/train/business/controller/admin/ConfirmOrderAdminController.java
@@ -0,0 +1,39 @@
+package org.jiegiser.train.business.controller.admin;
+
+import org.jiegiser.train.common.context.LoginMemberContext;
+import org.jiegiser.train.common.resp.CommonResp;
+import org.jiegiser.train.common.resp.PageResp;
+import org.jiegiser.train.business.req.ConfirmOrderQueryReq;
+import org.jiegiser.train.business.req.ConfirmOrderSaveReq;
+import org.jiegiser.train.business.resp.ConfirmOrderQueryResp;
+import org.jiegiser.train.business.service.ConfirmOrderService;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admin/confirm-order")
+public class ConfirmOrderAdminController {
+
+    @Resource
+    private ConfirmOrderService confirmOrderService;
+
+    @PostMapping("/save")
+    public CommonResp<Object> save(@Valid @RequestBody ConfirmOrderSaveReq req) {
+        confirmOrderService.save(req);
+        return new CommonResp<>();
+    }
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<ConfirmOrderQueryResp>> queryList(@Valid ConfirmOrderQueryReq req) {
+        PageResp<ConfirmOrderQueryResp> list = confirmOrderService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public CommonResp<Object> delete(@PathVariable Long id) {
+        confirmOrderService.delete(id);
+        return new CommonResp<>();
+    }
+
+}
---
+++ business/src/main/java/org/jiegiser/train/business/req/ConfirmOrderQueryReq.java
@@ -0,0 +1,12 @@
+package org.jiegiser.train.business.req;
+
+import org.jiegiser.train.common.req.PageReq;
+
+public class ConfirmOrderQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "ConfirmOrderQueryReq{" +
+                "} " + super.toString();
+    }
+}
