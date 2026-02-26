de755383de5233529a1dab7b08ac0af890e76162
jiegiser <changjie@jouav.com>
2026-02-26 14:13:59 +0800

    配置 Gateway + Nacos 实现按照应用来做路由转发

--- business/src/main/java/org/jiegiser/train/business/controller/TestController.java
+++ business/src/main/java/org/jiegiser/train/business/controller/TestController.java
@@ -1,12 +1,16 @@
 package org.jiegiser.train.business.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Autowired
+    Environment env;
     @GetMapping("/test")
     public String test() {
-        return "business test";
+        return "business test" + ":" + env.getProperty("server.port");
     }
 }
