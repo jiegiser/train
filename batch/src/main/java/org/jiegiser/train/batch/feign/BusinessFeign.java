637b1c9292a8054bb758b6fd5c6285167cd75876
jiegiser <changjie@jouav.com>
2026-03-02 08:36:02 +0800

    feat: 新增 Sentinel 降级处理方案

--- batch/src/main/java/org/jiegiser/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/org/jiegiser/train/batch/feign/BusinessFeign.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
 
+/**
+ * 采用 Sentinel 降级就会调用 fallback 方法
+ */
 // value 为服务名
 @FeignClient(value = "business", fallback = BusinessFeignFallback.class)
 // @FeignClient(name = "business", url = "http://127.0.0.1:8889/")
