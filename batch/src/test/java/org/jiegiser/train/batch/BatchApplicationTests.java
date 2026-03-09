31832ee8aae6f12ecd2de3bc7eee82e17a97b4d1
jiegiser <changjie@jouav.com>
2026-03-09 09:47:31 +0800

    chore: 修改打包配置

--- batch/src/test/java/org/jiegiser/train/batch/BatchApplicationTests.java
+++ batch/src/test/java/org/jiegiser/train/batch/BatchApplicationTests.java
@@ -1,9 +1,12 @@
 package org.jiegiser.train.batch;
 
+import org.jiegiser.train.batch.config.BatchApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+
+@SpringBootTest(classes = BatchApplication.class)
 class BatchApplicationTests {
 
     @Test
--- business/src/test/java/org/jiegiser/train/business/BusinessApplicationTests.java
+++ business/src/test/java/org/jiegiser/train/business/BusinessApplicationTests.java
@@ -1,9 +1,10 @@
 package org.jiegiser.train.business;
 
+import org.jiegiser.train.business.config.BusinessApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BusinessApplication.class)
 class BusinessApplicationTests {
 
     @Test
